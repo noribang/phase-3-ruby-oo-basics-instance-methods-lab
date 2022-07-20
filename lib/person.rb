e63fe61f31690bbcdf862432d1eeb083f90cf00e
hoonkim <hoonk9387@gmail.com>
2022-07-19 22:26:30 -0500

    Added Person class.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+end
+
+John = Person.new
+John.talk
+John.walk
+
