926276c8caad4d4e61e12433aeca69de476bee16
hoonkim <hoonk9387@gmail.com>
2022-07-19 22:21:16 -0500

    Added sit instance method.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+# Class body
+
+    # Instance method definition
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
+# bark
+
+snoopy = Dog.new
+snoopy.bark
